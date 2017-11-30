a6a91144fadfc0f4c89357d1747a9df534d0e279
tactfulness <pvenkat.rv@gmail.com>
2017-11-30 13:38:48 +0530

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn forecastapp.wsgi --log-file -
---
+++ forecastapp/urls.py
@@ -0,0 +1,25 @@
+"""forecastapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+
+from home. views import Home_view
+from contact. views import Contact_View
+urlpatterns = [
+	url(r'^$', Home_view, name="home"),
+	url(r'^contact/', Contact_View, name="contact"),
+    url(r'^admin/', admin.site.urls),
+]
