8881fe72ae8f2d0b3510ebb431d349e52fedc25c
tactfulness <pvenkat.rv@gmail.com>
2017-11-30 22:29:57 +0530

    updated staticfiles

--- forecastapp/settings/production.py
+++ forecastapp/settings/production.py
@@ -130,4 +130,4 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(os.path.join(os.path.join(BASE_DIR, 'staticfiles')))
+STATIC_ROOT = os.path.join(os.path.join(os.path.join(BASE_DIR, 'static')))
